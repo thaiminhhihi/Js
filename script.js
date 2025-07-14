2306c37b9cf7819cd084d609377f8fb1dd9788d9
Thái Minh <thaiminh30082006@gmail.com>
2025-07-14 23:32:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var addButton = document.getElementById('add-btn');
-    var jobList = document.getElementById('job-list');
+// - Bài tập: 
+// Khai báo mảng students rỗng.
+// Thêm các học sinh "An", "Bình", "Chi" vào mảng.
+// Học sinh "Bình" nghỉ học, hãy xoá bạn đó khỏi mảng.
+// Thêm "Dũng" vào đầu danh sách.
 
-    addButton.addEventListener('click', function() {
-       var jobName = document.createElement('li');
-       jobName.textContent = 'ten cong viec la ' + document.getElementById('job-name').value;
-        jobList.appendChild(jobName);
-var deleteButton = document.createElement('button');   
-deleteButton.textContent = 'Delete';
-deleteButton.addEventListener('click', function() {
-    jobList.removeChild(jobName);
+// In ra danh sách cuối cùng.
+let students = [];
+students.push("An", "Bình", "Chi"); // Thêm học sinh vào mảng
+students.splice(1,1); // Xoá "Bình" khỏi mảng
+students.unshift("Dũng"); // Thêm "Dũng" vào đầu mảng
+console.log(students); // In ra danh sách học sinh cuối cùng
+
+const fruits = ['Táo', 'Chuối', 'Cam'];
+fruits.forEach(function(fruit, index) {
+  console.log(index + ': ' + fruit);
 });
-       jobName.appendChild(deleteButton);
-    
-    });
